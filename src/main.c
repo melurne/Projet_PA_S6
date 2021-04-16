683b851be39c44486cd493d555495be4e06f0cda
melurne <maxence1.neus@gmail.com>
2021-04-16 18:03:20 +0200

    add first draft of structures

--- src/main.c
+++ src/main.c
@@ -2,3 +2,55 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#define MAX_LEN 100
+
+typedef struct Liste {
+  void* l[MAX_LEN]
+  int last;
+} Liste;
+
+typedef struct Airline {
+  char* IATA_code;
+  char* name;
+} Airline;
+
+typedef struct Airport {
+  char* IATA_code;
+  char* name;
+
+  char* city;
+  char* state;
+  char* country;
+  
+  float latitude;
+  float longitude;
+} Airport;
+
+typedef struct Flight {
+  int month;
+  int day;
+  int weekday;
+
+  Airline airline;
+  Airport org_air;
+  Airport dest_air;
+
+  int shed_dep;
+  float dep_delay;
+
+  float air_time;
+  int dist;
+
+  int shed_arr;
+  float arr_delay;
+
+  bool diverted;
+  bool canceled;
+} Flight;
+
+typedef Airline companies[];
+
+typedef struct Hashtable {
+
+
+} Haschtable;
