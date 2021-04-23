d7d345dbda8d74f3205f2282ad9b3b326e33e174
melurne <maxence1.neus@gmail.com>
2021-04-23 23:05:22 +0200

    Add .clang-format

--- src/main.c
+++ src/main.c
@@ -7,55 +7,64 @@
 
 #define MAX_LEN 100
 
-typedef struct Airline {
-  char *IATA_code;
-  char *name;
+typedef struct Airline
+{
+    char* IATA_code;
+    char* name;
 } Airline;
 
-typedef struct Airport {
-  char *IATA_code;
-  char *name;
+typedef struct Airport
+{
+    char* IATA_code;
+    char* name;
 
-  char *city;
-  char *state;
-  char *country;
+    char* city;
+    char* state;
+    char* country;
 
-  float latitude;
-  float longitude;
+    float latitude;
+    float longitude;
 } Airport;
 
-typedef struct Flight {
-  int month;
-  int day;
-  int weekday;
+typedef struct Flight
+{
+    int month;
+    int day;
+    int weekday;
 
-  char *airline;
-  char *org_air;
-  char *dest_air;
+    char* airline;
+    char* org_air;
+    char* dest_air;
 
-  int shed_dep;
-  float dep_delay;
+    int shed_dep;
+    float dep_delay;
 
-  float air_time;
-  int dist;
+    float air_time;
+    int dist;
 
-  int shed_arr;
-  float arr_delay;
+    int shed_arr;
+    float arr_delay;
 
-  bool diverted;
-  bool canceled;
+    bool diverted;
+    bool canceled;
 } Flight;
 
-typedef struct ListFlights {
-  void *l[MAX_LEN];
-  int last;
+typedef struct ListFlights
+{
+    void* l[MAX_LEN];
+    int last;
 } Liste;
 
-typedef struct HashtableAirlines {
+typedef struct HashtableAirlines
+{
 
 } TableAirlines;
 
-typedef struct HashtableAirports {
+typedef struct HashtableAirports
+{
 } TableAirports;
 
-int main() {}
+int
+main ()
+{
+}
